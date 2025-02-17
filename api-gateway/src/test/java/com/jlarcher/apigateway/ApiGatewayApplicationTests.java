e13403efc344482668a01ce19fddb7e219527cee
Jordan Larcher <lds.larcher@gmail.com>
2025-02-17 17:37:55 -0300

    Implementing API Gataway

---
+++ api-gateway/src/test/java/com/jlarcher/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlarcher.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
